7b2b3c665f0486f2cf04f08e025d85f8b0a753c4
Subhrajit Samanta <s.samanta.msus@gmail.com>
2026-06-16 09:21:26 +0530

    uploading solution of 3612. Process String with Special Operations I

---
+++ 3612. Process String with Special Operations I/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        ans = ""
+        for ch in s:
+            if ch == '*':
+                ans = ans[:-1]
+            elif ch == '#':
+                ans = ans + ans
+            elif ch == '%':
+                ans = ans[::-1]
+            else:
+                ans += ch
+        return ans
